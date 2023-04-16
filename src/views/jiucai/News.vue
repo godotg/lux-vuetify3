ca82d08dd3cb652a77d55ee610e01af7a4505abb
godotg <jaysunxiao@gmail.com>
2023-04-16 10:12:52 +0800

    update

--- src/views/jiucai/News.vue
+++ src/views/jiucai/News.vue
@@ -7,7 +7,6 @@ import _ from "lodash";
 import {useClipboard} from "@vueuse/core";
 import {useSnackbarStore} from "@/stores/snackbarStore";
 import {useDisplay} from "vuetify";
-import {searchPhotosApi} from "@/api/unsplashApi";
 
 const {copy} = useClipboard();
 const snackbarStore = useSnackbarStore();
@@ -15,9 +14,7 @@ const {mobile} = useDisplay()
 
 
 const newsRef = ref<News[]>([]);
-const hasMore = ref(true);
-const loading = ref(false);
-const scrollBottom = ref(0);
+const loadingRef = ref(false);
 
 
 const levelMap = {
@@ -54,54 +51,63 @@ onMounted(() => {
   setTimeout(() => {
     initNews();
   }, 1000);
+  setInterval(() => {
+    requestNews();
+  }, 5000)
 });
 
 async function initNews() {
   const request = new NewsRequest();
-  request.startId = 1;
+  request.startId = -1;
   request.endId = -1;
   const response: NewsResponse = await asyncAsk(request)
-  console.log("-------------------------");
-  console.log(response);
+  console.log("init:" + response);
   updateNewsRef(response.news)
 }
+
+async function requestNews() {
+  const firstNews = _.last(newsRef.value);
+  if (_.isEmpty(firstNews)) {
+    return;
+  }
+  const request = new NewsRequest();
+  request.startId = firstNews.id;
+  request.endId = -1;
+  const response: NewsResponse = await asyncAsk(request)
+  console.log("requestNews:" + response);
+  updateNewsRef(response.news);
+}
+
+async function loadMoreNews() {
+  const lastNews = _.last(newsRef.value);
+  if (_.isEmpty(lastNews)) {
+    snackbarStore.showErrorMessage("没有更多了");
+    return;
+  }
+  loadingRef.value = true;
+  const request = new NewsRequest();
+  request.startId = -1;
+  request.endId = lastNews.id;
+  const response: NewsResponse = await asyncAsk(request)
+  loadingRef.value = false;
+  console.log("loadMore:" + response);
+  updateNewsRef(response.news);
+}
+
 
 function updateNewsRef(news: Array<News>) {
   if (_.isEmpty(news)) {
     return;
   }
-  news.forEach((value, index) => {
-    if (_.findIndex(newsRef.value, (it) => it.id == value.id) < 0) {
-      newsRef.value = _.concat(value, newsRef.value)
-    }
+  news = _.filter(news, (it) => {
+    return _.findIndex(newsRef.value, (it1) => it1.id == it.id) < 0;
   });
+  let newNews = _.concat(newsRef.value, news);
+  newNews = _.sortBy(newNews, (it) => it.id);
+  newNews = _.reverse(newNews);
+  newsRef.value = newNews;
 }
 
-const loadMore = async () => {
-  loading.value = true;
-  console.log(hasMore.value);
-
-  if (!hasMore.value) {
-    loading.value = false;
-    snackbarStore.showMessage("没有更多了");
-    return;
-  }
-
-  // const photosResponse = await searchPhotosApi(queryOptions);
-  // const newList = photosResponse.data.results;
-  // hasMore.value = newList.length > 0;
-  // loading.value = false;
-};
-
-const onScroll = (e) => {
-  console.log("000000000000000000000000")
-  const target = e.target;
-  scrollBottom.value =
-    target.scrollHeight - target.scrollTop - target.clientHeight;
-  if (scrollBottom.value < 100) {
-    loadMore();
-  }
-};
 
 function copyNews(news: News) {
   let str = "";
@@ -133,7 +139,7 @@ function copyNews(news: News) {
 
 
 <template>
-  <div style="max-height: 2000px; overflow-y: auto" v-scroll.self="onScroll">
+  <v-container>
     <template v-if="mobile">
       <v-card v-for="newsEle in newsRef" class="mt-3" v-ripple @click="copyNews(newsEle)">
         <v-card-title>
@@ -166,46 +172,48 @@ function copyNews(news: News) {
         </v-card-actions>
       </v-card>
     </template>
-    <v-container v-else>
-      <v-timeline density="compact" side="end">
-        <template v-for="newsEle in newsRef">
-          <v-timeline-item fill-dot :dot-color="levelMap[newsEle.level].color" :size="levelMap[newsEle.level].size">
-            <template v-slot:icon>
-              <span>{{ newsEle.level }}</span>
-            </template>
-            <v-card v-ripple @click="copyNews(newsEle)">
-              <v-card-title>
-                <v-icon :color="levelMap[newsEle.level].color" :icon="levelMap[newsEle.level].icon"></v-icon>
-                级电报 {{ newsEle.ctime }}
-              </v-card-title>
-              <v-card-subtitle>
-                {{ newsEle.title }}
-              </v-card-subtitle>
-              <v-card-text>
-                {{ newsEle.content }}
-              </v-card-text>
-              <v-card-actions
-                v-if="!_.isEmpty(newsEle.stocks) || !_.isEmpty(newsEle.industries) || !_.isEmpty(newsEle.subjects)">
-                <template v-if="!_.isEmpty(newsEle.stocks)">
-                  <v-chip v-for="stock in newsEle.stocks" color="primary" size="x-small">
-                    {{ stock.name }} {{ stock.price }} / {{ stock.rise }}
-                  </v-chip>
-                </template>
-                <template v-if="!_.isEmpty(newsEle.industries)">
-                  <v-chip v-for="industry in newsEle.industries" color="primary" size="x-small" variant="outlined">
-                    {{ industry.name }} {{ industry.rise }}
-                  </v-chip>
-                </template>
-                <template v-if="!_.isEmpty(newsEle.subjects)">
-                  <v-chip v-for="subject in newsEle.subjects" size="x-small">
-                    {{ subject }}
-                  </v-chip>
-                </template>
-              </v-card-actions>
-            </v-card>
-          </v-timeline-item>
-        </template>
-      </v-timeline>
-    </v-container>
-  </div>
+    <v-timeline v-else density="compact" side="end">
+      <template v-for="newsEle in newsRef">
+        <v-timeline-item fill-dot :dot-color="levelMap[newsEle.level].color" :size="levelMap[newsEle.level].size">
+          <template v-slot:icon>
+            <span>{{ newsEle.level }}</span>
+          </template>
+          <v-card v-ripple @click="copyNews(newsEle)" max-width="1100px">
+            <v-card-title>
+              <v-icon :color="levelMap[newsEle.level].color" :icon="levelMap[newsEle.level].icon"></v-icon>
+              级电报 {{ newsEle.ctime }}
+            </v-card-title>
+            <v-card-subtitle>
+              {{ newsEle.title }}
+            </v-card-subtitle>
+            <v-card-text>
+              {{ newsEle.content }}
+            </v-card-text>
+            <v-card-actions
+              v-if="!_.isEmpty(newsEle.stocks) || !_.isEmpty(newsEle.industries) || !_.isEmpty(newsEle.subjects)">
+              <template v-if="!_.isEmpty(newsEle.stocks)">
+                <v-chip v-for="stock in newsEle.stocks" color="primary" size="x-small">
+                  {{ stock.name }} {{ stock.price }} / {{ stock.rise }}
+                </v-chip>
+              </template>
+              <template v-if="!_.isEmpty(newsEle.industries)">
+                <v-chip v-for="industry in newsEle.industries" color="primary" size="x-small" variant="outlined">
+                  {{ industry.name }} {{ industry.rise }}
+                </v-chip>
+              </template>
+              <template v-if="!_.isEmpty(newsEle.subjects)">
+                <v-chip v-for="subject in newsEle.subjects" size="x-small">
+                  {{ subject }}
+                </v-chip>
+              </template>
+            </v-card-actions>
+          </v-card>
+        </v-timeline-item>
+      </template>
+    </v-timeline>
+    <v-progress-linear v-if="loadingRef" indeterminate color="primary"></v-progress-linear>
+    <v-footer v-else v-ripple class="d-flex flex-column bg-grey-lighten-1" @click="loadMoreNews">
+      更多
+    </v-footer>
+  </v-container>
 </template>
