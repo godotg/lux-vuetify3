d1935e3352ebaa27662a421693ecbbdcc5816799
godotg <jaysunxiao@gmail.com>
2023-05-07 10:45:51 +0800

    update

--- src/components/toolbar/ToolbarNewsNotifications.vue
+++ src/components/toolbar/ToolbarNewsNotifications.vue
@@ -30,7 +30,7 @@ function refreshTotalNews() {
 
 <template>
   <v-btn v-if="totalNewsRef > 0" icon>
-    <v-badge :content="totalNewsRef" color="error">
+    <v-badge :content="totalNewsRef" color="primary">
       <v-icon>mdi-bell-outline</v-icon>
     </v-badge>
   </v-btn>
