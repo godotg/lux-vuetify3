4a1b0f321bbdd3a4e4868afd1b035fb4cfded338
godotg <jaysunxiao@gmail.com>
2023-06-01 23:14:49 +0800

    update

--- src/views/jiucai/News.vue
+++ src/views/jiucai/News.vue
@@ -227,7 +227,12 @@ function copyNews(news: News, event: Event) {
         <v-card v-if="newsStore.newsLevelFilterValue >= levelMap[newsEle.level].value" class="mt-3" v-ripple @click="copyNews(newsEle, $event)">
           <v-card-title>
             <v-icon :color="levelMap[newsEle.level].color" :icon="levelMap[newsEle.level].icon"></v-icon>
-            级情报 {{ newsEle.ctime }}
+            <template v-if="newsEle.level == 'C' || newsEle.level == 'D'">
+              级情报 {{ newsEle.ctime }}
+            </template>
+            <a v-else :href="'/ac/' + newsEle.id">
+              级情报 {{ newsEle.ctime }}
+            </a>
             <v-icon v-if="newsStore.isNew(newsEle.id)" color="primary" icon="mdi-new-box"></v-icon>
           </v-card-title>
           <v-card-subtitle>
@@ -297,7 +302,12 @@ function copyNews(news: News, event: Event) {
           <v-card v-ripple @click="copyNews(newsEle, $event)" max-width="1100px">
             <v-card-title>
               <v-icon :color="levelMap[newsEle.level].color" :icon="levelMap[newsEle.level].icon"></v-icon>
-              级情报 {{ newsEle.ctime }}
+              <template v-if="newsEle.level == 'C' || newsEle.level == 'D'">
+                级情报 {{ newsEle.ctime }}
+              </template>
+              <a v-else :href="'/ac/' + newsEle.id">
+                级情报 {{ newsEle.ctime }}
+              </a>
               <v-icon v-if="newsStore.isNew(newsEle.id)" color="primary" icon="mdi-new-box"></v-icon>
             </v-card-title>
             <v-card-subtitle>
